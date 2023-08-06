4192dc01679e274796b0a1e585544533d307b8f0
dalalaoye <dalalaoye@Demos-MacBook-Pro.local>
2023-08-06 20:03:22 +0800

    first commit

---
+++ da/package.cc
@@ -0,0 +1,58 @@
+{
+  "name": "dalekjs.com",
+  "version": "0.0.1",
+  "description": "A home for the DalekJS project",
+  "homepage": "http://dalekjs.com",
+  "directories": {
+    "test": "test"
+  },
+  "dependencies": {
+    "grunt": "~0.4.4",
+    "grunt-contrib-clean": "~0.5.0",
+    "grunt-contrib-copy": "~0.5.0",
+    "grunt-contrib-uglify": "~0.4.0",
+    "grunt-contrib-cssmin": "~0.9.0",
+    "grunt-contrib-htmlmin": "~0.2.0",
+    "grunt-contrib-compress": "~0.7.0",
+    "grunt-contrib-sass": "~0.7.3",
+    "grunt-rev": "~0.1.0",
+    "grunt-usemin": "~2.1.0",
+    "handlebars": "~2.0.0-alpha.2",
+    "grunt-contrib-jshint": "~0.9.2"
+  },
+  "repository": {
+    "type": "git",
+    "url": "http://github.com/dalekjs/dalekjs.com.git"
+  },
+  "bugs": {
+    "url": "https://github.com/dalekjs/dalekjs.com/issues"
+  },
+  "keywords": [
+    "dalekjs"
+  ],
+  "author": {
+    "name": "Sebastian Golasch",
+    "email": "public@asciidisco.com",
+    "url": "http://asciidisco.com"
+  },
+  "licenses": [
+    {
+      "type": "MIT",
+      "url": "https://github.com/dalekjs/dalek/blob/master/LICENSE-MIT"
+    }
+  ],
+  "engine": {
+    "node": ">=0.8"
+  },
+  "contributors": [
+    {
+      "name": "Anselm Hannemann",
+      "url": "http://github.com/https://github.com/anselmh"
+    }
+  ],
+  "devDependencies": {
+    "grunt-contrib-watch": "~0.6.1",
+    "connect-livereload": "~0.3.2",
+    "grunt-contrib-connect": "~0.7.1"
+  }
+}
